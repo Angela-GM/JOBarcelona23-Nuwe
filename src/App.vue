7d9655024d76b9ccb417c789a6fb6290e198e66e
Angela-GM <angelagm0112@gmail.com>
2023-03-24 16:49:47 +0100

    done api fetch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
+<template>
+  <section>
+    <input v-model="searchQuery" type="text" />
+    <button @click.prevent="fetchAPI">Submit</button>
+  </section>
+</template>
+
 <script setup>
-import { useApiStore } from "../src/stores/endpoints.js";
+import { ref } from "vue";
+// Variables api
+const apiUrl = import.meta.env.VITE_API_URL;
+const apiKey = import.meta.env.VITE_API_KEY;
+const searchQuery = ref("");
+
+//   Fectch API
+async function fetchAPI() {
+  const BASE_URL = `${apiUrl}?apiKey=${apiKey}&query='${searchQuery.value}'`;
+  console.log("Ha entrado en all fetch");
+
+  const response = await fetch(BASE_URL);
+  const data = await response.json(); // convertir el resultado en un json
+  console.log(data.results);
+  generateHTML(data.results);
+}
 </script>
 
-<template></template>
-
-<style scoped></style>
+<style></style>
--- src/api.js
+++
@@ -1,1 +0,0 @@
-export const apiKey = import.meta.env.API_KEY;
